fa9a92f87ee03fae822ab7f65d719db5ff6f4ba6
Bolin <abbf112233@gmail.com>
2024-07-20 01:41:21 +0800

    Delete 期中專題 directory

--- 期中專題/addres-book/connect-settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db_host = "127.0.0.1";
-$db_user = "root";
-$db_pass = "abbf112233";
-$db_name = "proj";
-$db_port = 3306;
--- 期中專題/addres-book/members_index_.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-if (isset($_SESSION["admin"])) {
-  include __DIR__ . '/members_list-admin.php';
-} else {
-  include __DIR__ . '/members_list-no-admin.php';
-}
